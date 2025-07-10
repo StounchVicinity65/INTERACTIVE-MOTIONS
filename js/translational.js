9147c5a311f262c79f532ceb57408df27dc2a4e8
StounchVicinity65 <wafashafowan@gmail.com>
2025-07-10 16:12:39 +0600

    Create translational.js

---
+++ js/translational.js
@@ -0,0 +1,20 @@
+let displacementInput, pos = 0, target = 100;
+
+function setup() {
+  createCanvas(windowWidth - 240, windowHeight);
+  displacementInput = document.getElementById('displacement');
+  noLoop();
+}
+
+function simulateTranslational() {
+  pos = 0;
+  target = float(displacementInput.value);
+  loop();
+}
+
+function draw() {
+  background(240);
+  fill(150, 100, 255);
+  ellipse(pos, height / 2, 40);
+  if (pos < target) pos += 2;
+}
